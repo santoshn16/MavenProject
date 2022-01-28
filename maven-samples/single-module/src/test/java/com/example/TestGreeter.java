0c566188a92a33d9d431748145af5faced92b0e9
santoshn16 <67142487+santoshn16@users.noreply.github.com>
2022-01-28 06:28:38 +0530

    JIRA 1.2.3
    Modified the Test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -36,7 +36,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "World, Thanks you making me Happy for Ever";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
